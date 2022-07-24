96a2c25bc7de948af887247539d1dca8d64e94e5
artem <apukhtin.artem@gmail.com>
2022-07-24 12:06:24 +0300

    first commit 'Hello World' p.2

---
+++ src/Hello_World/HelloWorld.java
@@ -0,0 +1,26 @@
+package Hello_World;
+
+public class HelloWorld {
+    public static void main(String[] args){
+        /**
+         *
+         * @author
+         * shift+alt+j   /**
+         *
+         */
+        System.out.println("Hello World");  //коммент
+        System.out.println(" ");
+        /*
+        @param
+        comment
+
+         */
+
+//        ctrl +/
+
+     /*   test      shift+ctrl+/
+                test
+                test
+                        */
+    }
+}
